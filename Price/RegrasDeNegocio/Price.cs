d83723fc93807ef4c40961cb553168a27bf16151
Olga Veronica <olgavas.opo@gmail.com>
2022-03-27 16:35:31 -0400

    criação do main e classe price

---
+++ Price/RegrasDeNegocio/Price.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TabelaPrice.RegrasDeNegocio
+{
+    public class Price
+    {        
+        public Price()
+        {
+            
+        Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}
+    
